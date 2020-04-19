9b65e8e2ce6fa473812d2eae246727b2984155cf
Norbert Baran <norbertbaran1@gmail.com>
2020-04-19 09:19:13 +0200

    Not passed test

---
+++ src/test/java/uj/jwzp2019/controller/SaveControllerTest.java
@@ -0,0 +1,28 @@
+package uj.jwzp2019.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SaveControllerTest {
+
+    @Test
+    public void saveToFilesWithDefaults(){
+        Assertions.assertThat("nieok").isEqualTo("ok");
+    }
+
+    @Test
+    public void saveToFilesWithChangedPrefix(){
+
+    }
+
+    @Test
+    public void saveToFilesWithChangedId(){
+
+    }
+
+    @Test
+    public void saveToFilesWithEmptyResult(){
+
+    }
+
+}
